f5ada4d92904588e9e3a64d2609a8976a304300d
Quynh <quynh.do@shipt.com>
2022-03-17 23:58:13 -0400

    fetch from freegan

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
-const express = require("express");
+// const express = require("express");
+// const { getParsedData } = require("./scraper/scraper.ts");
+import express from 'express';
+import { getParsedData }  from './scraper/scraper.js'
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
+    const data = getParsedData();
+    res.json({ message: 'hello' });
   });
 
 app.listen(PORT, () => {
